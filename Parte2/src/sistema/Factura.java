ead9ceb437982c4369c9d5fd10fb250faf20ec98
joemcara <94081168+joemcara@users.noreply.github.com>
2022-10-27 19:08:36 -0500

    class factura

---
+++ Parte2/src/sistema/Factura.java
@@ -0,0 +1,13 @@
+
+//Creamos factura para usos especificos
+public class Factura{
+
+    public void calcularImpuestosFactura(){
+        //calcula los impuestos
+    }
+    
+    public Factura generarFactura(){
+      //genera factura
+    }
+   
+}
