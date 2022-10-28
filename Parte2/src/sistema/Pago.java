bf1125e3b4ce60fa42f7de985367e8d67772df78
joemcara <94081168+joemcara@users.noreply.github.com>
2022-10-27 19:13:29 -0500

    Update Pago.java

--- Parte2/src/sistema/Pago.java
+++ Parte2/src/sistema/Pago.java
@@ -1,5 +1,9 @@
 package sistema;
-
+/*
+ * 2.  Class Pago viola el SRP -> porque posee metodos de calcular impuestos de factura y
+       generar factura, cuando realmente solo debe realizar cobro y notificar cuando realiza dicho cobro.
+       Es decir tiene responsabilidades que no le corresponden.
+ */
 public interface Pago {
     public void realizarCobro(double monto);
 }
