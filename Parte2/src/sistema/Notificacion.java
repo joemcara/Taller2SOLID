63ec5b42c4ce1fdfc2099b73b92c6c00e2b5240f
joemcara <94081168+joemcara@users.noreply.github.com>
2022-10-27 19:04:43 -0500

    Update Notificacion.java

--- Parte2/src/sistema/Notificacion.java
+++ Parte2/src/sistema/Notificacion.java
@@ -1,4 +1,8 @@
 package sistema;
+/*
+ * 1. Class Notificacion viola el OCP -> porque si se añade un metodo de pago adicional 
+ se tendrá que añadir otra condicion al if en el método notificar de Notificacion.
+*/
 
 public interface Notificacion {
     public void notificar();//notifica
