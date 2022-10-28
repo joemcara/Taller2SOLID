c1b2f22c7af0a961d40c064a2f83062d8f0f2a95
joemcara <94081168+joemcara@users.noreply.github.com>
2022-10-27 19:18:39 -0500

    Update PagoPaypal.java

--- Parte2/src/sistema/PagoPaypal.java
+++ Parte2/src/sistema/PagoPaypal.java
@@ -1,5 +1,12 @@
 package sistema;
 
+/*
+ * 4. Class Compra viola DIP -> porque tiene constructores para cada tipo de pago, 
+dependiendo asi de las clases de tipo pago que se creen y no de una interfaz que generalice todos estos
+tipos. 
+ * 
+ */
+
 public class PagoPaypal implements Pago, Notificacion {
     private boolean loggedIn;
     @Override
