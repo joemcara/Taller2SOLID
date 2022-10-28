b4f341c3f1b4eafb6a91213e41b64d0ba30d28d2
joemcara <joemcara@espol.edu.ec>
2022-10-27 19:33:26 -0500

    ejemplo de SRP

---
+++ Parte1/SRP.java
@@ -0,0 +1,27 @@
+/*
+Aqui se aplica el SRP -> ya que los metodos de guardar y sacar el carro 
+se los realiza en una clase aparte para que esta solo tenga esa responsabilidad y
+la clase carro no tenga ningun metodo que no sea del carro como tal
+*/
+class Carro{
+
+    String tipoCarro;
+    Public Carro(String tipo){
+        this.tipoCarro = tipo;
+    }
+
+    public getTipo(){
+        return tipoCarro;
+    }
+
+}
+
+class Garaje{
+    void guardarCarro(Carro carro){
+        //guarda el caro
+    }
+
+    void sacarCaro(Carro carro){
+        //saca el carro del garaje
+    }
+}
